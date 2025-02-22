234c2c57a8934d6121eb6f29a274c215588dec0c
hazelcodie <hazelmontiaora@gmail.com>
2025-02-22 20:38:51 +0800

    Changed the welcome page and added routes.

---
+++ resources/views/todolist/index.blade.php
@@ -0,0 +1,18 @@
+    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+ @extends('layouts.app')  
+
+ @section('content')
+   
+    {{-- <div class="bg-pink-200 rounded-2xl shadow-lg text-center w-full sm:max-w-md mt-6 px-6 py-6">
+        
+        <a href="{{ route('todolist.index') }}" class="bg-purple-500 text-white px-10 py-2 rounded-lg shadow-md font-medium transition-all hover:bg-purple-600 font-mono">
+            Definitely!
+        </a>
+        <a href="{{ route('todolist.index') }}" class="bg-purple-500 text-white px-10 py-2 rounded-lg shadow-md font-medium transition-all hover:bg-purple-600 font-mono">
+            Definitely!
+        </a>
+        <a href="{{ route('todolist.index') }}" class="bg-purple-500 text-white px-10 py-2 rounded-lg shadow-md font-medium transition-all hover:bg-purple-600 font-mono">
+            Definitely!
+        </a>
+    </div> --}}
+@endsection
